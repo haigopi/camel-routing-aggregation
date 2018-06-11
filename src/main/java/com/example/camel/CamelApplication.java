d4e36f21845fe4432b3326a3a54f39373a1c941b
Gopi <haigopi@gmail.com>
2018-06-10 21:57:17 -0500

    Initial Commit

---
+++ src/main/java/com/example/camel/CamelApplication.java
@@ -0,0 +1,12 @@
+package com.example.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelApplication.class, args);
+	}
+}
